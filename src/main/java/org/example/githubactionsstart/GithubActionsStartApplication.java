5b5899f14e864662a054cd41df3b231125b2e246
Lukas Balthazar <LBTurner@outlook.dk>
2025-02-06 10:02:28 +0100

    Initial commit

---
+++ src/main/java/org/example/githubactionsstart/GithubActionsStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubactionsstart/GithubActionsStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
