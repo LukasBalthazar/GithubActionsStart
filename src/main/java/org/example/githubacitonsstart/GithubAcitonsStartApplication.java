7d8f4744bb659100d66cedd47c256405f1ee8f8d
Lukas Balthazar <LBTurner@outlook.dk>
2025-06-28 17:42:41 +0200

    Initial commit

---
+++ src/main/java/org/example/githubacitonsstart/GithubAcitonsStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubacitonsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAcitonsStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubAcitonsStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubacitonsstart/GithubAcitonsStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubacitonsstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAcitonsStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
